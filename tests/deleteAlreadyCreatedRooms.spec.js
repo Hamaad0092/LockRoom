a64d56ce758df482f25392b3d72216808ffb411b
Hamaad Saeed <hamaadsaeed9712@gmail.com>
2025-02-11 16:09:42 +0500

    fixes

--- tests/deleteAlreadyCreatedRooms.spec.js
+++ tests/deleteAlreadyCreatedRooms.spec.js
@@ -14,15 +14,28 @@ test('Delete the rooms in the first organization.',async({page})=>{
     await page.getByRole('textbox', { name: 'Password' }).fill(password);
     await page.getByRole('button', { name: 'Sign In' }).click();
 
-    //*[@id="root"]/div[1]/div[2]/div[2]/div/div/div/div[1]
     await page.locator('xpath=//*[@id="root"]/div[1]/div[2]/div[2]/div/div/div/div[1]').click();
-    await page.locator('xpath=//*[@id="root"]/div[1]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/img[2]').click()
+    await page.locator('img[src="/icons/delete-icon.svg"][alt="Menu Icon"]').nth(0).click()
+    await expect(page.locator('//p[text()="ROOM_1"]/ancestor::div[contains(@class, "cursor-pointer")]')).toBeVisible();
+    console.log('✅ ROOM_1 exists');
+    console.log('🗑 Opened delete modal for ROOM_1');
     await page.locator('xpath=//*[@id="root"]/form/div/div/div[1]/input').fill(R1);
+    console.log(`📝 Entered confirmation text: ${R1}`);
     await page.locator('button[type="submit"]:has-text("Delete")').click();
+    await expect.soft(page.locator('//p[text()="ROOM_1"]/ancestor::div[contains(@class, "cursor-pointer")]')).toBeHidden();
+    console.log('✅ ROOM_1 deleted successfully');
     await page.waitForTimeout(5000)
 
-    await page.locator('div:has-text("ROOM_2") img[alt="Menu Icon"]').nth(1).click();
+    await page.locator('img[src="/icons/delete-icon.svg"][alt="Menu Icon"]').nth(0).click()
+    await expect(page.locator('//p[text()="ROOM_2"]/ancestor::div[contains(@class, "cursor-pointer")]')).toBeVisible();
+    console.log('✅ ROOM_2 exists');
+    console.log('🗑 Opened delete modal for ROOM_2');
     await page.locator('xpath=//*[@id="root"]/form/div/div/div[1]/input').fill(R2);
+    console.log(`📝 Entered confirmation text: ${R2}`);
     await page.locator('button[type="submit"]:has-text("Delete")').click();
+    await expect.soft(page.locator('//p[text()="ROOM_2"]/ancestor::div[contains(@class, "cursor-pointer")]')).toBeHidden();
+    console.log('✅ ROOM_2 deleted successfully');
     await page.waitForTimeout(5000)
+
+
 })
